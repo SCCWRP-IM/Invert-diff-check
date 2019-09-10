74b8719d1c06b19b100838d9f0e819343a63fe62
SCCWRP-IM <IT-Help@sccwrp.org>
2019-09-10 14:30:33 -0700

    I extracted and renamed the columns from the excel files

--- diffcheck.py
+++ diffcheck.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import numpy as np
 
-invert1 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_1.xlsx")
-invert2 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_2.xlsx")
+invert1 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_1.xlsx",sheet_name = 'Inverts_1')
+invert2 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_2.xlsx",sheet_name = 'Inverts_2')
+invert1.rename(columns ={'StationID_Trawl#':'stationid',
+                         'Species Name_TaxID':'speciesname',
+                         'Total Count':'count',
+                         'Vouchered':'voucher',
+                         'Qualifier':'qualifier',
+                         'Gross Weight (grams)': 'grossweight',
+                         'Tare Weight (grams)':'tareweight',
+                         'Net Weight (grams)':'netweight'}, inplace =True)
+invert2.rename(columns ={'StationID_Trawl#':'stationid',
+                         'Species Name_TaxID':'speciesname',
+                         'Total Count':'count',
+                         'Vouchered':'voucher',
+                         'Qualifier':'qualifier',
+                         'Gross Weight (grams)': 'grossweight',
+                         'Tare Weight (grams)':'tareweight',
+                         'Net Weight (grams)':'netweight'}, inplace =True)
