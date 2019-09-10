20806a4aa226c311745f6ad60f4bdd3f43579986
Robert Butler <r7butler@yahoo.com>
2019-09-10 11:01:48 -0700

    I read in the excel files as pandas dataframes so we can start comparing them

--- diffcheck.py
+++ diffcheck.py
@@ -1,5 +1,5 @@
-# This is the file where we will be writing all the code
-
 import pandas as pd
 import numpy as np
 
+invert1 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_1.xlsx")
+invert2 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_2.xlsx")
