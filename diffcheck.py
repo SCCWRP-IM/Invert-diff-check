7ce1a925b14f2919a77bbf5ab67c57b52853a22d
SCCWRP-IM <IT-Help@sccwrp.org>
2019-09-13 17:30:42 -0700

    Some changes

--- diffcheck.py
+++ diffcheck.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 df1 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_1.xlsx",sheet_name = 'Inverts_1')
 df2 = pd.read_excel("P:/PartTimers/DuyNguyen/Python Practice/pythonpractice/Bight18_Trawl_Data_2.xlsx",sheet_name = 'Inverts_2')
 df1.rename(columns ={'StationID_Trawl#':'stationid',
@@ -22,17 +23,31 @@ df1 = df1[['stationid','speciesname','count','voucher','qualifier','grossweight'
 df2 = df2[['stationid','speciesname','count','voucher','qualifier','grossweight','tareweight','netweight']]
 
 Merge = pd.merge(df1,df2, how ='outer', on = ['stationid','speciesname'], suffixes=('_1', '_2'))
-Merge.to_excel('P:\PartTimers\DuyNguyen\Python Practice\pythonpractice\Invert Check.xlsx')
-Merge['check counts']= Merge.apply( lambda x: (x.count_1 == x.count_2) | ((pd.isna(x.count_1)) & (pd.isna(x.count_2))) , axis =1  )
-Merge['check voucher']= Merge.apply( lambda x: (x.voucher_1 == x.voucher_2) | ((pd.isna(x.voucher_1)) & (pd.isna(x.voucher_2))), axis =1  )
-Merge['check qualifier']= Merge.apply( lambda x: (x.qualifier_1 == x.qualifier_2) | ((pd.isna(x.qualifier_1)) & (pd.isna(x.qualifier_2))), axis =1  )
-Merge['check grossweight']= Merge.apply( lambda x: (x.grossweight_1 == x.grossweight_2) | ((pd.isna(x.grossweight_1)) & (pd.isna(x.grossweight_2))), axis =1)
-Merge['check tareweight']= Merge.apply( lambda x: (x.tareweight_1 == x.tareweight_2) | ((pd.isna(x.tareweight_1)) & (pd.isna(x.tareweight_2))), axis =1  )
-Merge['check netweight']= Merge.apply( lambda x: (x.netweight_1 == x.netweight_2) | ((pd.isna(x.netweight_1)) & (pd.isna(x.netweight_2))), axis =1)
+#Merge.to_excel('P:\PartTimers\DuyNguyen\Python Practice\pythonpractice\Invert Check.xlsx')
+Merge['check_counts']= Merge.apply( lambda x: (x.count_1 == x.count_2) | ((pd.isna(x.count_1)) & (pd.isna(x.count_2))) , axis =1  )
+Merge['check_voucher']= Merge.apply( lambda x: (x.voucher_1 == x.voucher_2) | ((pd.isna(x.voucher_1)) & (pd.isna(x.voucher_2))), axis =1  )
+Merge['check_qualifier']= Merge.apply( lambda x: (x.qualifier_1 == x.qualifier_2) | ((pd.isna(x.qualifier_1)) & (pd.isna(x.qualifier_2))), axis =1  )
+Merge['check_grossweight']= Merge.apply( lambda x: (x.grossweight_1 == x.grossweight_2) | ((pd.isna(x.grossweight_1)) & (pd.isna(x.grossweight_2))), axis =1)
+Merge['check_tareweight']= Merge.apply( lambda x: (x.tareweight_1 == x.tareweight_2) | ((pd.isna(x.tareweight_1)) & (pd.isna(x.tareweight_2))), axis =1  )
+Merge['check_netweight']= Merge.apply( lambda x: (x.netweight_1 == x.netweight_2) | ((pd.isna(x.netweight_1)) & (pd.isna(x.netweight_2))), axis =1)
+Merge['In both files?']= Merge.apply( lambda x: (x.check_counts == 'FALSE') | ((pd.isna(x.netweight_1)) & (pd.isna(x.netweight_2))), axis =1)
 Merge = Merge[['stationid',
- 'speciesname','check counts','count_1','count_2','check voucher','voucher_1','voucher_2',
- 'check qualifier','qualifier_1','qualifier_2',
- 'check grossweight','grossweight_1','grossweight_2',
- 'check tareweight','tareweight_1','tareweight_2',
- 'check netweight','netweight_1','netweight_2']]
-Merge.to_excel('P:\PartTimers\DuyNguyen\Python Practice\pythonpractice\Invert Check.xlsx',index = False)
+ 'speciesname','check_counts','count_1','count_2','check_voucher','voucher_1','voucher_2',
+ 'check_qualifier','qualifier_1','qualifier_2',
+ 'check_grossweight','grossweight_1','grossweight_2',
+ 'check_tareweight','tareweight_1','tareweight_2',
+ 'check_netweight','netweight_1','netweight_2']]
+#Merge.to_excel('P:\PartTimers\DuyNguyen\Python Practice\pythonpractice\Invert Check.xlsx',index = False)
+species1 = set(zip(df1['stationid'].tolist(), df1['speciesname'].tolist()))
+species2 = set(zip(df2['stationid'].tolist(), df2['speciesname'].tolist()))
+
+#Below is the set of species that are in 1 but not 2
+A = species1 - species2
+
+#Below is the set of species that are in 2 but not 1
+B = species2 - species1
+Set1notSet2 =pd.DataFrame(A)
+Set1notSet2.rename(columns ={0:"Stationid_1",1:"Speciesname_1"}, inplace = True)
+Set2notSet1 =pd.DataFrame(B)
+Set2notSet1.rename(columns ={0:"Stationid_2",1:"Speciesname_2"}, inplace = True)
+Merge['In 1 not In 2'] = Merge.apply(lambda x: x.Stationid_1 = x.stationid)
